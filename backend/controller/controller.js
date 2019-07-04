3dddbd9e64e010eb7c61a8872820efba5d2d400a
Trix <trbelleza@up.edu.ph>
2019-07-04 15:23:36 +0800

    Added backend POST (add item)

--- backend/controller/controller.js
+++ backend/controller/controller.js
@@ -7,3 +7,16 @@ exports.get_all_items = (req, res, next) => {
 		res.send(result);
 	});
 };
+
+exports.add_item = (req, res, next) => {
+	const data = {
+		name : req.body.name,
+		qty : req.body.qty,
+		amount : req.body.amount
+	};
+	const query_string = 'INSERT INTO items (name, qty, amount) VALUES (?, ?, ?)';
+	db.query(query_string, [data.name, data.qty, data.amount], (err, result) => {
+		if (err) console.log(err);
+		else res.send(data);
+	})
+}
--- backend/router/router.js
+++ backend/router/router.js
@@ -4,5 +4,6 @@ const items_controller = require(__dirname + '/../controller/controller');
 
 module.exports = (router) => {
 	router.get('/view-all-items', items_controller.get_all_items);
+	router.post('/add-item', items_controller.add_item);
 	return router
 }
