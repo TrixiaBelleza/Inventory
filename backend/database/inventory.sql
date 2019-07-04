3da20ffc55d1990077dd61c5ab88d82dc2037060
Trix <trbelleza@up.edu.ph>
2019-07-04 10:32:28 +0800

    Finished setup database and initial frontend

---
+++ backend/database/inventory.sql
@@ -0,0 +1,13 @@
+-- Create a database called “inventory", with table “items”, with columns (id, name, qty, amount)
+
+drop database if exists inventory;
+create database inventory;
+use inventory;
+
+create table items (
+	id INT NOT NULL AUTO_INCREMENT,
+	name VARCHAR(200),
+	qty INT,
+	amount INT,
+	constraint id_pk primary key(id)
+);
